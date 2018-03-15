3716473f5ffc19a945912f046950d2e1f2fcf82d
pdme <philip.ebels@gmail.com>
2018-03-15 15:58:10 +0000

    Use reduceRight instead of reverse and reduce

--- src/compound.js
+++ src/compound.js
@@ -1,6 +1,4 @@
 const compound = (...fns) => (...args) =>
-  fns
-    .reverse()
-    .reduce((result, fn) => fn(...[result, ...args.slice(1)]), args[0])
+  fns.reduceRight((result, fn) => fn(...[result, ...args.slice(1)]), args[0])
 
 module.exports = compound
