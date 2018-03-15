fe86d1447568ffbfee3919cff564bb3d8a4e99c8
pdme <philip.ebels@gmail.com>
2018-03-15 10:54:24 +0000

    Refactored, add build step

--- src/compound.js
+++ src/compound.js
@@ -1,9 +1,6 @@
 const compound = (...fns) => (...args) =>
   fns
     .reverse()
-    .reduce(
-      (result, fn) => fn.apply(null, [result].concat(args.slice(1))),
-      args[0],
-    )
+    .reduce((result, fn) => fn(...[result, ...args.slice(1)]), args[0])
 
 module.exports = compound
